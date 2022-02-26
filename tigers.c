94c6fb84e3e528477bd84513d886e92529bccadb
MidlightDDK <majed.azar7@gmail.com>
2022-02-26 14:52:47 +0000

    Added some code inside tigers.c

---
+++ tigers.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+int main(int argc, char argv**)
+{
+	// Output a message in the terminal
+	printf("This is just a test fam");
+
+	// Keep the console opened
+	system("PAUSE");
+	return 0;
+}
